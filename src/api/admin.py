5eb657f1b6e71a7bd895b0ddefbb3a8a543581bf
IRemsha <meww.mailbox@gmail.com>
2020-09-02 16:07:58 +0500

    update title for admin panel and off overwrite for AWS

--- src/api/admin.py
+++ src/api/admin.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .auth.models import APIUser
 
+admin.site.site_title = 'project_name administration'
+admin.site.site_header = 'project_name administration'
+admin.site.enable_nav_sidebar = False
+
+
 admin.site.register(APIUser)
 
 # Register your models here.
