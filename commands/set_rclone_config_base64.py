8cc0a9644bcb88ec3c791d8e3aaa121f568eac55
lololozhkin <alexand3r.lozhkin@gmail.com>
2021-05-07 18:33:56 +0500

    endpoints instead of manual urls are now in project. auto settings with some providers. only digitalocean and aws are available now

--- commands/set_rclone_config_base64.py
+++ commands/set_rclone_config_base64.py
@@ -7,13 +7,44 @@ import os
 ENV_FILE = os.path.join('src', os.environ.get('ENV_FILE', '.env'))
 
 
+def get_digital_ocean_endpoint():
+    location = os.environ.get('AWS_LOCATION')
+    endpoint = f'{location}.digitaloceanspaces.com'
+
+    return endpoint
+
+
+def get_aws_endpoint():
+    location = os.environ.get('AWS_LOCATION')
+    endpoint = f's3.{location}.amazonaws.com'
+
+    return endpoint
+
+
+OPTIONS = {
+    'DigitalOcean': get_digital_ocean_endpoint(),
+    'AWS': get_aws_endpoint(),
+}
+
+
 def main():
     with open('./.rclone.conf') as f:
         template = Template(f.read())
 
-    substituted = template.safe_substitute(**os.environ)
+    provider = os.environ.get('AWS_PROVIDER')
+    if provider not in OPTIONS:
+        raise ValueError(
+            f'{provider} is not allowed. available providers are: {", ".join(OPTIONS.keys())}'
+        )
+
+    endpoint = OPTIONS[provider]
+    vars = os.environ.copy()
+    vars['AWS_ENDPOINT'] = endpoint
+
+    substituted = template.safe_substitute(**vars)
     base64_conf = base64.b64encode(substituted.encode()).decode()
-    dotenv.set_key(ENV_FILE, "RCLONE_CONFIG_BASE64", base64_conf, quote_mode='never')
+    dotenv.set_key(ENV_FILE, 'RCLONE_CONFIG_BASE64', base64_conf, quote_mode='never')
+    dotenv.set_key(ENV_FILE, 'AWS_ENDPOINT', endpoint, quote_mode='never')
 
 
 if __name__ == '__main__':
